5f86bfae37cd9472107e1ad1170d1d690a067349
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-05-02 14:18:47 +0200

    add: img folder

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+    data() {
+        return {
+            menu: [
+                {
+                    option: CHARACTERS,
+                    isActive: false
+                },
+                {
+                    option: COMICS,
+                    isActive: false
+                },
+                {
+                    option: MOVIES,
+                    isActive: false
+                },
+                {
+                    option: TV,
+                    isActive: false
+                },
+                {
+                    option: GAMES,
+                    isActive: false
+                },
+                {
+                    option: COLLECTIBLES,
+                    isActive: false
+                },
+                {
+                    option: VIDEOS,
+                    isActive: false
+                },
+                {
+                    option: FANS,
+                    isActive: false
+                },
+                {
+                    option: NEWS,
+                    isActive: false
+                },
+                {
+                    option: SHOP,
+                    isActive: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="logo"></div>
+    <div>
+        <ul>
+            <li v-for="option in menu"><a href=""></a></li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
